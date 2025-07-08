ae2b7f85197b591eb9990828853799f1b68fb1f4
Dipendragiri2001 <Dipendragiri2001@gmail.com>
2025-07-08 12:12:25 +0545

    conenction string

--- CoursePro.API/Program.cs
+++ CoursePro.API/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var config = builder.Configuration.GetConnectionString("DefaultConnection");
+var config = builder.Configuration.GetValue<string>("DefaultConnection");
 builder.Services.AddInfrastructureServices(config);
 var app = builder.Build();
 
