6272699908a0cce9c500736fcec300085d249e29
gurau-anil <gurau.anil@gmail.com>
2025-07-18 14:50:10 -0700

    Ant Design Experiment

--- app/coursepro/src/layouts/admin-layout.tsx
+++ app/coursepro/src/layouts/admin-layout.tsx
@@ -1,9 +1,78 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
+import { Layout, Menu, theme } from 'antd';
+import React from "react";
+import './styles/admin-layout.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookOpen, faGauge, faUserAlt } from "@fortawesome/free-solid-svg-icons";
+
+const { Header, Content, Sider } = Layout;
+
+const navItems = [ 
+    {key: 'dashboard', value: faGauge, label: 'Dashboard' }, 
+    {key: 'courses', value: faBookOpen, label: 'Courses' },
+    {key: 'students', value: faUserAlt, label: 'Students' },
+    ].map(
+  (icon) => ({
+    key: icon.key,
+    icon: React.createElement(FontAwesomeIcon, { icon: icon.value }),
+    label: icon.label,
+  }),
+);
+
 
 function AdminLayout() {
+
+    const {
+        token: { colorBgContainer },
+      } = theme.useToken();
+
+      let selectedNavItem: string[] = ['dashboard']
+      const location = useLocation();
+    
+      let currentactive = navItems.find(c=> location.pathname.includes(c.label.toLowerCase()))
+    
+      if(currentactive){
+        selectedNavItem = currentactive.key.endsWith("admin")? ['dashboard'] : currentactive? [currentactive.key] : [];
+      }
+      const navigate = useNavigate();
+
+      function onMenuSelect(data: any){
+        console.log(data)
+        navigate(data.key == 'dashboard'? '': data.key)
+    
+      }
+      
     return (  
     <>
-        <Outlet></Outlet>
+    <Layout className="layout">
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+      
+      <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsible 
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+        className="side-nav"
+      >
+        <Menu theme="dark" 
+            mode="inline" 
+            defaultSelectedKeys={selectedNavItem} 
+            onSelect={onMenuSelect}
+            items={navItems} />
+      </Sider>
+        <Content>
+          <div className="content-section">
+            <Outlet></Outlet>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+        
     </>
     );
 }
